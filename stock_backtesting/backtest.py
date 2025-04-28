036a864ec76ff32c7584c307430b17bda08b759b
Maciej Adamski <madamskip1@gmail.com>
2025-04-28 20:10:42 +0200

    Fix calculating assets value when skipping candlesticks

--- stock_backtesting/backtest.py
+++ stock_backtesting/backtest.py
@@ -34,6 +34,10 @@ class Backtest:
 
         candlesticks_to_skip = self.__strategy.candletsticks_to_skip()
         for _ in range(candlesticks_to_skip):
+            self.__account.update_assets_value(
+                self.__data.get_current_data_index(), 0.0
+            )
+            self.__account.calculate_equity(self.__data.get_current_data_index())
             self.__data.increment_data_index()
 
         for _ in range(self.__strategy.candletsticks_to_skip(), len(self.__data)):
