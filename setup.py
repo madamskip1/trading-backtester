615fbc71bb2e1059d2fdfe4aff5f74a5aed43f74
Maciej Adamski <madamskip1@gmail.com>
2025-05-03 21:05:41 +0200

    Add few fields to setup.py about lib

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import setup
 setup(
     name="trading-backtester",
     version="0.0.1a1",
-    install_requires=["numpy"],
+    description="A trading backtesting framework for Python",
+    author="Adamski Maciej",
+    author_email="madamskip1@gmail.com",
     packages=["trading_backtester"],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy>=1.16.0",
+    ],
 )
