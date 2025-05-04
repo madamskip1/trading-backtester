f603af7e4ffc06f3434b3a709347e459d52c4dfc
Maciej Adamski <madamskip1@gmail.com>
2025-05-04 22:12:45 +0200

    Version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="trading-backtester",
-    version="0.2.0a1",
+    version="0.2.0",
     description="A trading backtesting framework for Python",
     author="Adamski Maciej",
     author_email="madamskip1@gmail.com",
