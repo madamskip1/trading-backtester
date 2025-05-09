2f8d821c25df5f1e89e04b6e7f7908d6f3eae4fb
Maciej Adamski <madamskip1@gmail.com>
2025-05-09 19:04:24 +0200

    Split main drawing method into smaller draw_x_plot methods

--- trading_backtester/plotting.py
+++ trading_backtester/plotting.py
@@ -32,34 +32,13 @@ class Plotting:
         ax_price = ax[1]
         ax_volume = ax[2]
 
-        datetime_to_index: Dict[np.datetime64, int] = {
-            dt: i for i, dt in enumerate(self.__data.datetime)
-        }
+        x_axis_values = np.arange(len(self.__data))
 
-        for i, data in enumerate(self.__data):
-            self.__draw_candlestick(
-                ax_price, i, data["open"], data["high"], data["low"], data["close"]
-            )
-            self.__draw_volume_bar(
-                ax_volume,
-                i,
-                data["volume"],
-                self.__get_bar_color(data["open"], data["close"]),
-            )
-
-        for trade in reversed(self.__trades):
-            if trade.trade_type == TradeType.CLOSE:
-                assert isinstance(trade, CloseTrade)
-                self.__draw_closed_trade(ax_price, trade, datetime_to_index)
-
+        self.__draw_price_plot(ax_price)
+        self.__draw_volume_plot(ax_volume)
         self.__draw_equity_plot(ax_equity)
-        ax_price.set_ylabel("Price")
-        ax_price.grid(True, axis="y")
 
-        ax_volume.set_ylabel("Volume")
-        ax_volume.grid(True, axis="y")
-
-        ax_volume.set_xticks(list(range(len(self.__data))))
+        ax_volume.set_xticks(x_axis_values)
 
         dateformat = self.__get_date_format()
         xticklabels = [
@@ -76,6 +55,135 @@ class Plotting:
         fig.autofmt_xdate()
 
         plt.show()
+
+    def __draw_price_plot(self, ax: Axes):
+        ax.set_ylabel("Price")
+        ax.grid(True, axis="y"),
+
+        self.__draw_candlesticks(ax)
+        self.__draw_closed_trades(ax)
+
+    def __draw_volume_plot(self, ax: Axes):
+        ax.set_ylabel("Volume")
+        ax.grid(True, axis="y")
+
+        for x, data in enumerate(self.__data):
+            color = self.__get_bar_color(data["open"], data["close"])
+            ax.bar(
+                x, data["volume"], width=0.95, color=color, edgecolor="black", zorder=2
+            )
+
+    def __draw_equity_plot(
+        self,
+        ax: Axes,
+    ):
+        ax.set_ylabel("Equity")
+
+        ax_right = ax.twinx()
+        ax_right.set_ylabel("Equity %")
+        ax.grid(True, axis="y")
+
+        ax.plot(self.__account.get_equity(), zorder=11, color="blue")
+
+        percent_equity = (
+            self.__account.get_equity() / self.__account.get_equity()[0] * 100
+        )
+        ax_right.set_ylim(
+            percent_equity.min() - percent_equity.min() * 0.05,
+            percent_equity.max() + percent_equity.max() * 0.05,
+        )
+        formatter = FuncFormatter(lambda y, _: f"{y:.1f}%")
+        ax_right.yaxis.set_major_formatter(formatter)
+
+        peaks = np.maximum.accumulate(self.__account.get_equity())
+        drawdowns_percentages = np.abs((self.__account.get_equity() - peaks) / peaks)
+        drawdown_threshold = 0.02
+        in_drawdown = False
+        last_peak = 0
+        max_drawdown = 0.0
+        for i, drawdown in enumerate(drawdowns_percentages):
+            if drawdown == 0.0:
+                if in_drawdown:
+                    if max_drawdown >= drawdown_threshold:
+                        ax.axvspan(last_peak, i - 1, color="red", alpha=0.2, zorder=1)
+                    in_drawdown = False
+                    max_drawdown = 0.0
+
+                last_peak = i
+            else:
+                in_drawdown = True
+                max_drawdown = max(max_drawdown, drawdown)
+
+    def __draw_candlesticks(self, ax: Axes):
+        for x, data in enumerate(self.__data):
+            candlestick_color = self.__get_bar_color(data["open"], data["close"])
+
+            # Draw the high and low lines
+            ax.plot(
+                [x, x], [data["low"], data["high"]], color=candlestick_color, zorder=2
+            )
+
+            body_low = min(data["open"], data["close"])
+            body_height = abs(data["close"] - data["open"])
+            body_bottom_left = (x - self.CANDLESTICK_WIDTH / 2, body_low)
+
+            # Draw the body of the candlestick
+            ax.add_patch(
+                Rectangle(
+                    body_bottom_left,
+                    self.CANDLESTICK_WIDTH,
+                    body_height,
+                    facecolor=candlestick_color,
+                    edgecolor="black",
+                    zorder=3,
+                )
+            )
+
+    def __draw_closed_trades(self, ax: Axes):
+        datetime_to_x_axis: Dict[np.datetime64, int] = {
+            dt: x for x, dt in enumerate(self.__data.datetime)
+        }
+        for trade in reversed(self.__trades):
+            if trade.trade_type == TradeType.CLOSE:
+                assert isinstance(trade, CloseTrade)
+                self.__draw_closed_trade(ax, trade, datetime_to_x_axis)
+
+    def __draw_closed_trade(
+        self,
+        ax: Axes,
+        close_trade: CloseTrade,
+        datetime_to_x_axis: Dict[np.datetime64, int],
+    ):
+        assert close_trade.close_datetime is not None
+        assert close_trade.close_price is not None
+
+        x_start = datetime_to_x_axis[close_trade.open_datetime]
+        y_start = close_trade.open_price
+        x_end = datetime_to_x_axis[close_trade.close_datetime]
+        y_end = close_trade.close_price
+
+        marker = "^" if close_trade.position_type == PositionType.LONG else "v"
+        color = self.__get_bar_color(close_trade.open_price, close_trade.close_price)
+
+        # Draw the line from the open to the close
+        ax.plot(
+            [x_start, x_end],
+            [y_start, y_end],
+            color="black",
+            linewidth=2,
+            linestyle="--",
+            zorder=4,
+        )
+        # Draw the marker at the close
+        ax.plot(
+            x_end,
+            y_end,
+            marker=marker,
+            color=color,
+            zorder=5,
+            markeredgecolor="black",
+            markersize=10,
+        )
 
     def __get_bar_color(self, open_val: float, close_val: float) -> str:
         return (
@@ -98,102 +206,3 @@ class Plotting:
             return "%Y-%m"
         else:
             return "%Y"
-
-    def __draw_candlestick(
-        self,
-        ax: Axes,
-        i: int,
-        open_val: float,
-        high_val: float,
-        low_val: float,
-        close_val: float,
-    ):
-        lower = min(open_val, close_val)
-        height = abs(close_val - open_val)
-        bar_color = self.__get_bar_color(open_val, close_val)
-
-        ax.plot([i, i], [low_val, high_val], color="black", zorder=2)
-        ax.add_patch(
-            Rectangle(
-                (i - self.CANDLESTICK_WIDTH / 2, lower),
-                self.CANDLESTICK_WIDTH,
-                height,
-                facecolor=bar_color,
-                zorder=3,
-            )
-        )
-
-    def __draw_volume_bar(self, ax: Axes, i: int, volume_val: float, color: str):
-        ax.bar(i, volume_val, width=0.95, color=color, zorder=2)
-
-    def __draw_closed_trade(
-        self,
-        ax: Axes,
-        close_trade: CloseTrade,
-        datetime_to_index: Dict[np.datetime64, int],
-    ):
-        assert close_trade.close_datetime is not None
-        assert close_trade.close_price is not None
-
-        x_start = datetime_to_index[close_trade.open_datetime]
-        y_start = close_trade.open_price
-        x_end = datetime_to_index[close_trade.close_datetime]
-        y_end = close_trade.close_price
-
-        marker = "^" if close_trade.position_type == PositionType.LONG else "v"
-        color = self.__get_bar_color(close_trade.open_price, close_trade.close_price)
-
-        ax.plot(
-            [x_start, x_end],
-            [y_start, y_end],
-            color="black",
-            linewidth=2,
-            linestyle="--",
-            zorder=4,
-        )
-        ax.plot(
-            x_end,
-            y_end,
-            marker=marker,
-            color=color,
-            zorder=5,
-            markeredgecolor="black",
-            markersize=10,
-        )
-
-    def __draw_equity_plot(
-        self,
-        ax: Axes,
-    ):
-        ax.plot(self.__account.get_equity(), zorder=11, color="blue")
-
-        percent_equity = (
-            self.__account.get_equity() / self.__account.get_equity()[0] * 100
-        )
-        ax2 = ax.twinx()
-        ax2.set_ylim(
-            percent_equity.min() - percent_equity.min() * 0.05,
-            percent_equity.max() + percent_equity.max() * 0.05,
-        )
-        formatter = FuncFormatter(lambda y, _: f"{y:.1f} %")
-        ax2.yaxis.set_major_formatter(formatter)
-        ax2.grid(True, axis="y")
-
-        peaks = np.maximum.accumulate(self.__account.get_equity())
-        drawdowns_percentages = np.abs((self.__account.get_equity() - peaks) / peaks)
-        drawdown_threshold = 0.02
-        in_drawdown = False
-        last_peak = 0
-        max_drawdown = 0.0
-        for i, drawdown in enumerate(drawdowns_percentages):
-            if drawdown == 0.0:
-                if in_drawdown:
-                    if max_drawdown >= drawdown_threshold:
-                        ax.axvspan(last_peak, i - 1, color="red", alpha=0.2, zorder=1)
-                    in_drawdown = False
-                    max_drawdown = 0.0
-
-                last_peak = i
-            else:
-                in_drawdown = True
-                max_drawdown = max(max_drawdown, drawdown)
