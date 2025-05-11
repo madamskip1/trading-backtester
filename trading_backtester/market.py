13e806be22cda05f953f5c1c8b7207353bcfa3a8
Maciej Adamski <madamskip1@gmail.com>
2025-05-11 20:23:07 +0200

    Make market methods more obvious to user

--- trading_backtester/market.py
+++ trading_backtester/market.py
@@ -19,22 +19,21 @@ class Market:
             raise ValueError(
                 "Candlestick is not closed yet. Can't look into the future."
             )
-
         return self.__data.get_current_data("close")
 
     def get_current_low_price(self) -> float:
-        return (
-            self.__data.get_current_data("low")
-            if self.__data.get_candlestick_phase() == CandlestickPhase.CLOSE
-            else self.__data.get_current_data("open")
-        )
+        if self.__data.get_candlestick_phase() == CandlestickPhase.OPEN:
+            raise ValueError(
+                "Candlestick is at open phase. Can't look into the future."
+            )
+        return self.__data.get_current_data("low")
 
     def get_current_high_price(self) -> float:
-        return (
-            self.__data.get_current_data("high")
-            if self.__data.get_candlestick_phase() == CandlestickPhase.CLOSE
-            else self.__data.get_current_data("open")
-        )
+        if self.__data.get_candlestick_phase() == CandlestickPhase.OPEN:
+            raise ValueError(
+                "Candlestick is at open phase. Can't look into the future."
+            )
+        return self.__data.get_current_data("high")
 
     def get_open_price_on_nth_ago(self, n: int) -> Optional[float]:
         if n < 1:
@@ -53,3 +52,21 @@ class Market:
             return None
 
         return self.__data[self.__data.get_current_data_index() - n]["close"]
+
+    def get_low_price_on_nth_ago(self, n: int) -> Optional[float]:
+        if n < 1:
+            raise ValueError("To look into the past, n must be greater than 0.")
+
+        if self.__data.get_current_data_index() - n < 0:
+            return None
+
+        return self.__data[self.__data.get_current_data_index() - n]["low"]
+
+    def get_high_price_on_nth_ago(self, n: int) -> Optional[float]:
+        if n < 1:
+            raise ValueError("To look into the past, n must be greater than 0.")
+
+        if self.__data.get_current_data_index() - n < 0:
+            return None
+
+        return self.__data[self.__data.get_current_data_index() - n]["high"]
